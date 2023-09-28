26558496dbf3cddb756cee75561dd7571902a399
Truong Van Quy <truongvanquy2k3@gmail.com>
2023-09-28 11:28:01 +0700

    cai dat handler create

--- app/config/index.js
+++ app/config/index.js
@@ -4,7 +4,7 @@ const config = {
         port: process.env.PORT || 3000,
     },
     db: {
-        uri: process.env.MONGODB_URI || "mongodb://localhost:27017/contactbook"
+        uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook"
     }
 }
 
--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,5 +1,23 @@
-module.exports.create = (req, res) => {
-    res.send({ message: 'Create handler' })
+const ApiError = require("../api-error")
+const ContactService = require("../services/contact.service")
+const MongoDB = require('../utils/mongodb.util')
+
+module.exports.create = async (req, res, next) => {
+
+    if (!req.body?.name) {
+        return next(new ApiError(400, "Name cannot be empty"))
+    }
+
+    try {
+        const contactService = new ContactService(MongoDB.client)
+        const document = await contactService.create(req.body)
+        return res.send(document)
+    } 
+    catch (err) {
+        return next (
+            new ApiError(500, "An error occurrd while creating the contact")
+        )
+    }
 }
 
 module.exports.findAll = (req, res) => {
